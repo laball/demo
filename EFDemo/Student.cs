ebdbd2129f997251a7a00d20b9a473e5d67909d6
unknown <libo@zone1.hz.net>
2016-06-06 13:45:23 +0800

    删除Bin，obj文件夹，提交EF Demo工程

---
+++ EFDemo/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo
+{
+    public class Student
+    {
+
+    }
+
+    public class StudentMapping :EntityTypeConfiguration<Student>
+    {
+
+
+    }
+
+
+}

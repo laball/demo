d9a055b6dd7cb6b91e3501c198034fe594036f44
Lee <libo1573@163.com>
2016-10-19 23:16:37 +0800

    添加Jil JSON序列化器测试代码

---
+++ JilDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JilDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var times = new List<long>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var obj = new
+                {
+                    ID = Guid.NewGuid().ToString(),
+                    Name = new { ID = i, Code = "Code" },
+                    Code = "Code",
+                    Items = new[]
+                    {
+                        new  { ID = i, Name = "Name1" },
+                        new { ID = i+1, Name = "Name1" },
+                        new { ID = i+2, Name = "Name1" }
+                    }
+                };
+
+                var watch = new Stopwatch();
+                watch.Start();
+
+                var json = Jil.JSON.Serialize(obj);
+
+                watch.Stop();
+
+                var time = watch.ElapsedMilliseconds;
+                times.Add(time);
+            }
+
+            Trace.WriteLine(times.Sum() / (times.Count * 1.0));
+        }
+    }
+}

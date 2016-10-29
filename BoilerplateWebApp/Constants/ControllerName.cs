213c35b1fa542c16de85ec5536e7a1892e6c9760
Lee <laball1573@163.com>
2016-10-29 16:40:53 +0800

    添加ABPDemo工程

---
+++ BoilerplateWebApp/Constants/ControllerName.cs
@@ -0,0 +1,8 @@
+﻿namespace BoilerplateWebApp.Constants
+{
+    public static class ControllerName
+    {
+        public const string Error = "Error";
+        public const string Home = "Home";
+    }
+}

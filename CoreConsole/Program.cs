884b016b46d7650db5f7680c23d17b380a2a79c5
Lee <libo1573@163.com>
2016-12-11 21:03:29 +0800

    添加.net core console demo 代码

---
+++ CoreConsole/Program.cs
@@ -0,0 +1,45 @@
+using System;
+using Dapper;
+using Newtonsoft.Json;
+
+namespace CoreConsole
+{
+    public class Program
+    {
+        public const string ConnectionString = "server='Lee';database='tese';uid='sa';pwd ='@LiBo#8923052'";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (var connection = new System.Data.SqlClient.SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    var sql = "select * FROM People";
+
+                    var people = connection.Query<People>(sql);
+
+                    foreach (var person in people)
+                    {
+                        Console.WriteLine(JsonConvert.SerializeObject(person));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+            }
+        }
+    }
+
+    public class People
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime? DeletionTime { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? LastModificationTime { get; set; }
+    }
+}

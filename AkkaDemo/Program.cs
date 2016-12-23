d2a9cd68cbe955d42e49fd4c000a8d1220909194
Lee <laball1573@163.com>
2016-12-23 20:38:49 +0800

    添加Akka测试代码

---
+++ AkkaDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace AkkaDemo
+{
+
+    public class GreetingMessage
+    {
+        public string Name { get; set; }
+    }
+
+    public class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            Receive<GreetingMessage>(c => { Console.WriteLine("Hello " + c.Name); });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("Test");
+
+            var greeter = system.ActorOf<GreetingActor>();
+            greeter.Tell(new GreetingMessage() { Name = "Lee" });
+
+            Console.ReadLine();
+        }
+    }
+}
